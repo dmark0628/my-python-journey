971cafbe248c3d687a83e8daf75d164d0672a7e9
DanMark <dmark0628@gmail.com>
2025-08-30 23:50:43 +0800

    Add my files

---
+++ Hello World.py
@@ -0,0 +1,17 @@
+# print Hello World
+
+print("Hello World")
+
+#print input string
+fname = input("What is your first name? ")
+lname = input("What is you last name? ")
+
+#input integers
+age = int(input("How old are you? "))
+
+#print input name name
+print("Hi my name is",fname,lname,"I'm",age "years old")
+
+
+
+
